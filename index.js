e7d66a1f525ecc846a04ce030e4008dd87ef2a3e
Cristián Andrés Frías García <cristianfrias@MacBook-Air-de-Cristian.local>
2024-11-19 21:53:33 -0300

    Listados usando relaciones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const conexion = require("./conexion.js");
 const Producto = require("./models/Producto.entity.js");
+const TipoProducto = require("./models/TipoProducto.entity.js");
 
 // EJEMPLO ASÍNCRONO
 // conexion.sync().then(() => {
@@ -11,12 +12,54 @@ const Producto = require("./models/Producto.entity.js");
         await conexion.sync();
         console.log("Conexión a Base de Datos EXITOSA");
 
-        const listadoProducto = await Producto.findAll();
+        // // CREACIÓN DE TIPOS DE PRODUCTOS
+        // const tipoRopa = await TipoProducto.create({ nombre: "Ropa" });
+        // const tipoAlimento = await TipoProducto.create({ nombre: "Alimentos" });
+        // console.log("Tipos de productos creados con éxito");
         
-        console.log(listadoProducto);
+
+        // // CREACIÓN DE PRODUCTOS
+        // await Producto.create({ nombre: "Polera", valor: 9_000, tipo_producto_id: tipoRopa.id});
+        // await Producto.create({ nombre: "Jean", valor: 15_000, tipo_producto_id: tipoRopa.id});
+        // await Producto.create({ nombre: "Arroz", valor: 2_500, tipo_producto_id: tipoAlimento.id});
+        // console.log("Productos creados con éxito");
+
+        // const listadoProductos = await Producto.findAll({
+        //     where: {
+        //         tipo_producto_id: 1
+        //     },
+        //     order: [
+        //         ["valor", "DESC"]
+        //     ],
+        //     attributes: ["id","nombre","valor"],
+        //     include: {
+        //         model: TipoProducto,
+        //         as: "tipoProducto",
+        //         attributes: ["id"]
+        //     }
+        // });
+        // console.table(JSON.parse(JSON.stringify(listadoProductos)));
+
+        // const listadoTipos = await TipoProducto.findAll({
+        //     include:{
+        //         model: Producto,
+        //         as: "productos"
+        //     }
+        // });
+
+        const listadoTipos = await TipoProducto.findOne({
+            where: {
+                id: 2
+            },
+            include:{
+                model: Producto,
+                as: "productos"
+            }
+        });
+        console.log(JSON.parse(JSON.stringify(listadoTipos)));
+
     } catch (error) {
         console.log(error.message);
     }
-    
     
 })()
